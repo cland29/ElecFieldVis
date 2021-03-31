6da130f1d3184b93d569625e68b7aec39fb0cee5
cland29 <carl.lee.landskron@gmail.com>
2021-03-31 17:04:27 -0400

    Start adding methods to Parse
    Abandon implementing for the time being due to compolation errors

---
+++ Lab01/Parser.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include <json.hpp>
+#include <string>
+#include <fstream>
+
+#include <iostream>
+
+using namespace std;
+using json = nlohmann::json;
+
+class Parser{
+    public:
+        void parserJson(string fileName){
+            //Read JSON file
+            //std::ifstream i(fileName);
+            //json j;
+            //i >> j;
+            //string s = j.dump();
+            //printf("Printing the Json file:\n%s\n-----------\n", s);
+
+        }
+
+    
+
+
+};
--- Lab01/Parser.h
+++ Lab01/Parser.h
@@ -1,1 +1,17 @@
 #pragma once
+#include<stdio.h>
+#include <json.hpp>
+#include <string>
+
+#include <iostream>
+
+using namespace std;
+
+class Parser{
+    public:
+        void parserJson(string fileName);
+
+    
+
+
+};
